811c472e336c46a1e2e64d083014032d7f7e18b5
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-08-18 14:36:15 -0400

    debugging devoured burgers

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,9 +1,9 @@
+var burger = require("../models/burger.js");
 var express = require("express");
-
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
+
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
@@ -32,9 +32,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function(result) {
+  burger.update(condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,5 @@
 // Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
+var orm = require("../config/orm");
 
 var burger = {
   all: function(cb) {
@@ -13,16 +13,16 @@ var burger = {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
+  update: function(condition, cb) {
+    orm.update("burgers", {devoured: true}, condition, function(res) {
       cb(res);
     });
   },
-//   delete: function(condition, cb) {
-//     orm.delete("burgers", condition, function(res) {
-//       cb(res);
-//     });
-//   }
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
 };
 
 // Export the database functions for the controller (burgersController.js).
