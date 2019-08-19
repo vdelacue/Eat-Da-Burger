be0632f8b4928c1ffc660726dc528f7e9bf12cf0
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-08-18 23:44:22 -0400

    debugging heroku

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
- connection =
-  mysql.createConnection({
-  host: "pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "a5xv1vftb55k2q1v	",
-  password: "sm1br1kwzkrveskm",
-  database: "ip044ftyne2kfr21"
-});
+  connection =
+    mysql.createConnection({
+      host: "pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      port: 3306,
+      user: "a5xv1vftb55k2q1v	",
+      password: "sm1br1kwzkrveskm",
+      database: "ip044ftyne2kfr21"
+    });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
